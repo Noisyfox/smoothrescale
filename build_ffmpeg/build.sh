52d4ef1a3d22250ad9927dd09f55dd6ccc8bd2ae
Noisyfox <timemanager.rick@gmail.com>
2017-08-25 13:18:05 +1000

    Refine build.sh

--- build_ffmpeg/build.sh
+++ build_ffmpeg/build.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
+
 #NDK=$HOME/Desktop/adt/android-ndk-r9
-NDK=$ANDROID_NDK
-SYSROOT=$NDK/platforms/android-23/arch-arm/
-TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+NDK=${ANDROID_NDK}
+PREFIX=$(pwd)/build/android
+
 function build_one
 {
+cd ffmpeg
+
 ./configure\
-    --prefix=$PREFIX\
-    --enable-shared\
+    --prefix=${PREFIX}/${CPU}\
+    --disable-everything\
+    --disable-all\
     --disable-static\
-    --disable-doc\
-    --disable-ffmpeg\
-    --disable-ffplay\
-    --disable-ffprobe\
-    --disable-ffserver\
-    --disable-avdevice\
-    --disable-doc\
-    --disable-symver\
-    --disable-avcodec\
-    --disable-avdevice\
-    --disable-avfilter\
-    --disable-avformat\
-    --disable-postproc\
-    --disable-swresample\
-    --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi-\
+    --disable-asm\
+    --enable-shared\
+    --enable-avutil\
+    --enable-swscale\
+    --cross-prefix=${TOOLCHAIN_PREFIX}\
     --target-os=linux\
-    --arch=arm\
+    --arch=${ARCH}\
     --enable-cross-compile\
-    --sysroot=$SYSROOT\
-    --extra-cflags="-Os -fpic $ADDI_CFLAGS"\
-    --extra-ldflags="$ADDI_LDFLAGS"\
-    $ADDITIONAL_CONFIGURE_FLAG
+    --sysroot=${SYSROOT}\
+    --extra-cflags="-Os -fpic ${ADDI_CFLAGS}"\
+    --extra-ldflags="${ADDI_LDFLAGS}"\
+    ${ADDITIONAL_CONFIGURE_FLAG}
 
 make clean
 make
 make install
+
+cd ../
 }
+
 CPU=arm
-PREFIX=$(pwd)/build/android/$CPU
+ARCH=arm
+SYSROOT=$NDK/platforms/android-23/arch-${CPU}/
+TOOLCHAIN=arm-linux-androideabi
+TOOLCHAIN_EXEC=arm-linux-androideabi
+
+HOST=darwin-x86_64
+TOOLCHAIN_PREFIX=${NDK}/toolchains/${TOOLCHAIN}-4.9/prebuilt/${HOST}/bin/${TOOLCHAIN_EXEC}-
 ADDI_CFLAGS="-marm"
-cd ffmpeg
+
 build_one
